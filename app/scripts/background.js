1ad3adec41d5b688a36b62bd3f5a87ed87f07ded
Bronson McKinley <gitbronsonmckinley@gmail.com>
2016-04-05 12:29:51 -0700

    Rough draft of simple application switcher functionality
    When the Command key is held down while the Alt or Control
    keys are pressed, a list of dom elements is created and appended to
    the body of the active tab. The elements contain the titles of the tabs.
    When the Command key is released, the elements are removed.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,1 +1,92 @@
-console.log('background');
+'use strict';
+
+console.log('background.js');
+
+(function() {
+
+  var tabIds = [];
+
+  function contentListener(request, sender, sendResponse) {
+    if (request.tabObjects) {
+      getOrderedTabs(function(tabs) {
+        // reduce the tab objects to minimal info
+        sendResponse({ tabObjects: tabs });
+      });
+      return true;
+    }
+  }
+
+  chrome.runtime.onMessage.addListener(contentListener);
+
+  function updateTabIds(tabId) {
+    var index = tabIds.indexOf(tabId);
+    if (index === -1) {
+      tabIds.unshift(tabId);
+    } else {
+      tabIds.unshift(tabIds.splice(tabIds.indexOf(tabId), 1)[0]);
+    }
+  }
+
+  function getOrderedTabs(next) {
+    chrome.tabs.query({}, function(tabs) {
+      var tabsById = {};
+      for (var i = 0; i < tabs.length; i++) {
+        var tab = tabs[i];
+        tabsById[tab.id] = tab;
+      }
+
+      var orderedTabs = [];
+      for (var j = 0; j < tabIds.length; j++) {
+        var tabId = tabIds[j];
+        orderedTabs.push(tabsById[tabId]);
+      }
+
+      next(orderedTabs);
+    });
+  }
+
+  function getTabs() {
+    chrome.tabs.query({}, function(tabs) {
+      for (var i = 0; i < tabs.length; i++) {
+        var tab = tabs[i];
+        if (tabIds.indexOf(tab.id) === -1) {
+          tabIds.push(tab.id);
+        }
+      }
+    });
+  }
+
+  function getWindow(next) {
+    chrome.windows.getCurrent({ populate: true }, function(window) {
+      var tabs = window.tabs;
+      var activeTab;
+      for (var i = 0; i < tabs.length; i++) {
+        var tab = tabs[i];
+        if (tab.active) {
+          activeTab = tab;
+        } else {
+          tabIds.push(tab.id);
+        }
+      }
+      tabIds.unshift(activeTab.id);
+
+      next();
+    });
+  }
+
+  getWindow(getTabs);
+
+  chrome.tabs.onCreated.addListener(function(tab) {
+    console.log('chrome.tabs.onCreated');
+  });
+
+  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    console.log('chrome.tabs.onRemoved');
+  });
+
+  chrome.tabs.onActivated.addListener(function(activeInfo) {
+    console.log('chrome.tabs.onActivated');
+    updateTabIds(activeInfo.tabId);
+  });
+
+})();
--- app/scripts/content.js
+++ app/scripts/content.js
@@ -1,1 +1,66 @@
-console.log('contentscript');
+'use strict';
+
+(function() {
+
+  console.log('content.js');
+
+  var displayCon;
+
+  function onKeyDown(e) {
+    if (e.metaKey) {
+      if (e.keyIdentifier == 'Alt') {
+        displayTabObjects();
+        // rotate through tabs
+      } else if (e.keyIdentifier == 'Control') {
+        displayTabObjects();
+        // rotate through tabs
+      }
+    }
+  }
+
+  function onKeyUp(e) {
+    if (e.keyIdentifier == 'Meta') {
+      // select new tab
+      distroyTabObjects();
+    }
+  }
+
+  document.addEventListener('keydown', onKeyDown);
+  document.addEventListener('keyup', onKeyUp);
+
+  function tabObjects(next) {
+    chrome.runtime.sendMessage({ tabObjects: true }, function(response) {
+      next(response.tabObjects);
+    });
+  }
+
+  function displayTabObjects() {
+    if (!displayCon) {
+      tabObjects(display);
+    }
+  }
+
+  function distroyTabObjects() {
+    if (displayCon) {
+      document.body.removeChild(displayCon);
+      displayCon = undefined;
+    }
+  }
+
+  function display(tabObjects) {
+    displayCon = document.createElement('div');
+    var l = tabObjects.length;
+    for (var i = 0; i < l; i++) {
+      var tabObject = tabObjects[i];
+      // tabObject.favIconUrl
+      // tabObject.url
+      var tabCon = document.createElement('div');
+      var tabTitle = document.createTextNode(tabObject.title);
+      var tabIcon = document.createElement('div');
+      displayCon.appendChild(tabCon);
+      tabCon.appendChild(tabTitle);
+      document.body.appendChild(displayCon);
+    }
+  }
+
+})();
