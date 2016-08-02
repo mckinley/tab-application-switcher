ffb12e1ec8100d46bd5c4effb3e40d0f50726589
Bronson McKinley <gitbronsonmckinley@gmail.com>
2016-08-02 01:55:49 -0600

    share options classes between display and extension preferences

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,30 +1,6 @@
-import defaultOptions from './lib/default-options.json';
+import EventEmitter from 'events';
+import Options from './lib/options';
 
-function save() {
-  var keyModifier = document.getElementById('key-modifier').value;
-  var keyNext = document.getElementById('key-next').value;
-  var keyPrevious = document.getElementById('key-previous').value;
-  chrome.storage.sync.set({
-    keys: {
-      modifier: keyModifier,
-      next: keyNext,
-      previous: keyPrevious
-    }
-  }, function() {
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 2000);
-  });
-}
-
-function init() {
-  chrome.storage.sync.get(defaultOptions, (storage) => {
-    document.getElementById('key-modifier').value = storage.keys.modifier;
-    document.getElementById('key-next').value = storage.keys.next;
-    document.getElementById('key-previous').value = storage.keys.previous;
-  });
-}
-document.addEventListener('DOMContentLoaded', init);
-document.getElementById('save').addEventListener('click', save);
+let eventEmitter = new EventEmitter();
+let options = new Options(eventEmitter);
+document.querySelector('.TAS_optionsCon').appendChild(options.render());
